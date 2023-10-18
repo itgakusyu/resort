7ea5e5ae76f87ca3f3d7d17e12b0656742d6da97
itgakusyu <144197909+itgakusyu@users.noreply.github.com>
2023-10-18 13:14:29 +0900

    initial commit
    練習

---
+++ js/scrpt.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+  let menu = document.querySelector('#menu-icon');
+  let navbar = document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+  };
+}
